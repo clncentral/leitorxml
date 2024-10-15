ff302be363db5902ca573417f934385bc0463e6f
Elias <cln.central@gmail.com>
2024-10-15 08:50:55 -0300

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -83,6 +83,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	adicionarTopico("Formiga desmotivada", "hist/a_demissao_da_formiga.html", "2024-04-20");
 	adicionarTopico("Consulta CNPJ", "hist/cnpj.html", "2024-05-09");
 	adicionarTopico("Chat", "hist/chat.html", "2024-05-08");
-	adicionarTopico("Chat", "hist/radio.html", "2024-05-08");
+	adicionarTopico("Radios", "hist/radio.html", "2024-05-08");
     // Adicionar mais tópicos conforme necessário
 });
