0dd5625ead473551e66f0d9df3b95fc1725ff513
Elias <cln.central@gmail.com>
2025-04-12 11:04:41 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'meu-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icone.png',
+  '/manifest.json'
+];
+
+// Instala o service worker e faz cache dos arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercepta requisições e responde com cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
