64641be3438aed2a0eab90ba2abd4bab22d11e7f
Elias <cln.central@gmail.com>
2025-05-23 14:15:48 -0300

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -91,6 +91,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	adicionarTopico("Youtube", "https://www.youtube.com/", "2024-05-08");
 	adicionarTopico("SpotFy", "https://open.spotify.com/playlist/5wMbIGMnM1Dpj2BcCHZZlO", "2024-04-20");
 	adicionarTopico("Radio", "hist/radio.html", "2024-04-22");
+	adicionarTopico("Ler PDF", "hist/novo6.html", "2024-04-22");
 	
     // Adicionar mais tópicos conforme necessário
 });
