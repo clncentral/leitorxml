4e9eb24594466fe45d3311ad468cd57dc9c74a4a
Elias <cln.central@gmail.com>
2025-10-30 15:44:25 -0300

    Update script2.js

--- script/script2.js
+++ script/script2.js
@@ -95,11 +95,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	adicionarTopico("Manuais", "Classificacao Ativos Imobilizados", "blue/Classificacao_Ativos_Imobilizados.xlsx", "2024-04-22");
 	adicionarTopico("Manuais", "Correção Despesa para Ativo", "blue/CorrecaoDespesaAtivo.docx", "2024-04-22");
 	adicionarTopico("Manuais", "Cadastro Ativo Imobilizado", "blue/cadastroAtivo.docx", "2024-04-22");
+	adicionarTopico("Manuais", "Pedido de Compra Ativo - Danfe", "https://ajuda.bluesoft.com.br/faq/compra-de-ativo-imobilizado", "2024-04-22");
 
 
     adicionarTopico("Favoritos", "Spotify", "https://open.spotify.com/", "2024-04-22");
     adicionarTopico("Favoritos", "Rádio Online", "hist/radio.html", "2024-04-20");
 });
+
 
 
 
